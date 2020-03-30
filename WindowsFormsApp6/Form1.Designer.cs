6e67c28e6933c41829d54e9d540891eaad7ecc43
Patryk Krawczak <patryk.krawczak@lingarogroup.com>
2020-03-30 19:41:20 +0200

    Add project files.

---
+++ WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.k_box = new System.Windows.Forms.TextBox();
+            this.m_box = new System.Windows.Forms.TextBox();
+            this.wynik_box = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // k_box
+            // 
+            this.k_box.Location = new System.Drawing.Point(36, 157);
+            this.k_box.Name = "k_box";
+            this.k_box.Size = new System.Drawing.Size(100, 26);
+            this.k_box.TabIndex = 0;
+            // 
+            // m_box
+            // 
+            this.m_box.Location = new System.Drawing.Point(36, 234);
+            this.m_box.Name = "m_box";
+            this.m_box.Size = new System.Drawing.Size(100, 26);
+            this.m_box.TabIndex = 1;
+            // 
+            // wynik_box
+            // 
+            this.wynik_box.Location = new System.Drawing.Point(340, 103);
+            this.wynik_box.Multiline = true;
+            this.wynik_box.Name = "wynik_box";
+            this.wynik_box.Size = new System.Drawing.Size(404, 157);
+            this.wynik_box.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(340, 293);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(404, 99);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "OBLICZ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Patryk krawczak zadanie 6 c#";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "wartosc k";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 200);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "wartosc m";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.wynik_box);
+            this.Controls.Add(this.m_box);
+            this.Controls.Add(this.k_box);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox k_box;
+        private System.Windows.Forms.TextBox m_box;
+        private System.Windows.Forms.TextBox wynik_box;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsApp6/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            float true_wynik = 1000000 / 3;
+
+
+            double n = double.Parse(k_box.Text);
+            n = Math.Pow(10, n);
+            int m = int.Parse(m_box.Text);
+            Random rnd = new Random();
+
+            double f = 0.0F;
+            int o = 0;
+            WindowsFormsApp.SingleCount single = new SingleCount();
+            single.X1 = 0;
+            single.X2 = 100;
+            Random rmd = new Random();
+            double min = Double.MaxValue;
+            double min2 = Double.MaxValue;
+            double minx11 = 0F;
+            double minx12 = 0F;
+            double minx21 = 0F;
+            double minx22 = 0F;
+            double minx111 = 0F;
+            double minx112 = 0F;
+            double minx121 = 0F;
+            double minx122 = 0F;
+            for (int j = 0; j < m; j++)
+            {
+
+
+                f = 0.0F;
+                int licz = 0;
+                double old_i = 0;
+
+                
+                single.X1 = rmd.Next(0,50);
+                single.X2 = rmd.Next(50, 100);
+
+                for (double i = single.X1; i <= single.X2; i = i + 100F / n)
+                {
+
+                    licz += 1;
+                    f += (i * i) * (i - old_i);
+
+                    old_i = i;
+                };
+                double f1 = 0.0F;
+
+                old_i = 0;
+
+                double x1old = single.X1;
+                double x2old = single.X2;
+
+                single.X1 = rmd.Next(0, 50);
+                single.X2 = rmd.Next(50, 100);
+
+                for (double i = single.X1; i <= single.X2; i = i + 100F / n)
+                {
+
+
+                    f1 += (i * i * i) * (i - old_i);
+
+                    old_i = i;
+                };
+                double roznica = Math.Abs(f - f1);
+                if (roznica < min) 
+                {
+                    min = roznica;
+                    minx11 = x1old;
+                    minx12 = x2old;
+                    minx21 = single.X1;
+                    minx22 = single.X2;
+                }
+                f = 0.0F;
+
+                single.X1 = rmd.Next(0, 50);
+                single.X2 = rmd.Next(50, 100);
+                old_i = 0;
+                for (double i = 0.0F; i <= 100.0; i = i + 100F / n)
+                {
+
+                    licz += 1;
+                    f += (((i * i) + (old_i * old_i)) / 2) * (i - old_i);
+
+                    old_i = i;
+                };
+
+
+                f1 = 0.0F;
+                single.X1 = rmd.Next(0, 50);
+                single.X2 = rmd.Next(50, 100);
+                old_i = 0;
+                for (double i = 0.0F; i <= 100.0; i = i + 100F / n)
+                {
+
+                    licz += 1;
+                    f1 += (((i * i) + (old_i * old_i)) / 2) * (i - old_i);
+
+                    old_i = i;
+                };
+
+                roznica = Math.Abs(f - f1);
+                if (roznica < min2)
+                {
+                    min = roznica;
+                    minx111 = x1old;
+                    minx112 = x2old;
+                    minx121 = single.X1;
+                    minx122 = single.X2;
+                }
+            };
+
+            wynik_box.Text += "  prostokąty: x1 = " + minx11.ToString() + " x2 = " + minx12.ToString() + " x1 = " + minx21.ToString() + " x2 = " + minx22.ToString() + " roznica = " + min.ToString();
+            wynik_box.Text += "  trapezy: x1 = " + minx111.ToString() + " x2 = " + minx112.ToString() + " x1 = " + minx121.ToString() + " x2 = " + minx122.ToString() + " roznica = " + min2.ToString();
+
+        }
+    }
+}
